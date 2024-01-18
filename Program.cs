a98d0240d4de90b3e69ba77aaa3eb5b49f5086fb
albyyasmeens <156489829+albyyasmeens@users.noreply.github.com>
2024-01-17 16:05:56 -0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+using System;
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string GRULower = "Gru";
+        string GRUUpper = "GRU";
+        Console.WriteLine(GRUUpper);
+        Console.WriteLine("No, no, no!");
+        Console.WriteLine(GRUUpper);
+        Console.WriteLine("Ok, my turn");
+
+        Console.WriteLine("He said \"What??!\"\n");
+Console.WriteLine("And I said \"I Know!\"");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
+
+internal record struct NewStruct(object Item1, object Item2)
+{
+    public static implicit operator (object, object)(NewStruct value)
+    {
+        return (value.Item1, value.Item2);
+    }
+
+    public static implicit operator NewStruct((object, object) value)
+    {
+        return new NewStruct(value.Item1, value.Item2);
+    }
+}
