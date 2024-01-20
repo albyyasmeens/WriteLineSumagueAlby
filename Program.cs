1e49c468f18114154b2a4f46c68b9a249baa6989
albyyasmeens <156489829+albyyasmeens@users.noreply.github.com>
2024-01-19 19:41:22 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+internal partial class Program
+{
+    public static string AGNESUpper = "AGNES";
+    static string MARGOUpper = "MARGO";
+    static string GRUUpper = "GRU";
+    static string GRULower = "Gru";
+    static string EDITHUpper = "EDITH";
+    static string VENDORUpper = "VENDOR";
+    static string VENDORLower = "vendor";            
+
+    static void Main(string[] args)
+    {
+        
+        
+        Console.WriteLine(AGNESUpper + "(O.S)");
+        Console.WriteLine("\"Oh my gosh look at that fluffy unicorn!\"\n");
+        Console.WriteLine("A FLUFFY UNICORN");
+        Console.WriteLine("is the main prize at the shooting gallery.");
+        Console.WriteLine(AGNESUpper);
+        Console.WriteLine("\"He's so fluffy I'm gonna die!\"");
+            Console.WriteLine(MARGOUpper);
+                Console.WriteLine("\"You gotta let us play for it!\"");
+                Console.WriteLine(GRUUpper);
+                Console.WriteLine("\"No, no, no, no, no!\"");
+                Console.WriteLine(AGNESUpper);
+                Console.WriteLine("\"Come on!\"");
+                Console.WriteLine(GRULower + "sighs and looks at the vendor.");
+                Console.WriteLine(GRUUpper);
+                Console.WriteLine("\"How much for the fluffy unicorn?\"");
+                Console.WriteLine(VENDORUpper);
+                Console.WriteLine("(chuckles)\"Well...it is not for sale. But all you gotta do to win it is knock down that little spaceship there.\"");
+                Console.WriteLine("The vendor points to the smallest of the spaceship targets.");
+                Console.WriteLine(VENDORUpper);
+                Console.WriteLine("\"It's easy!\"");
+                Console.WriteLine(GRULower + "He slaps a dollar bill on the table. The vendor slurps on his drink, and presses a button that opens the curtains and readies the cork guns.");
+                Console.WriteLine(AGNESUpper);
+                Console.WriteLine("\"Yay!\"");
+                Console.WriteLine("They take aim and open fire! POP! POP! POP! They all miss.");
+                Console.WriteLine(AGNESUpper);
+                Console.WriteLine("\"Again! Again!\"");
+                Console.WriteLine(MARGOUpper);
+                Console.WriteLine("\"Wait, wait, wait!\"");
+                Console.WriteLine(EDITHUpper);
+                Console.WriteLine("\"Ah, come on, one more time.\"");
+                Console.WriteLine(AGNESUpper);
+                Console.WriteLine("\"Just one more time, I accidentally closed my eyes!\"");
+                Console.WriteLine(GRULower+ "slaps down another bill. POP! POP! POP! This time Agnes's cork hits the target.");
+                Console.WriteLine(AGNESUpper);
+                Console.WriteLine("\"I hit it! I hit it! Did you see that? I hit it!\"");
+                Console.WriteLine("The video sign above the game reads: “YOU LOSE.” Gru reacts.");
+                Console.WriteLine(EDITHUpper);
+                Console.WriteLine("\"Awww!\"");
+                Console.WriteLine(GRUUpper);
+                Console.WriteLine("\"Whoa, whoa, whoa... What was that? She hit that! I saw that with my own eyes!\"");
+                Console.WriteLine(VENDORLower + "gives Gru a smarmy smile. He flips through a stack of dollar bills.");
+                Console.WriteLine(VENDORUpper + ";");
+                Console.WriteLine("\"Hey, buddy. Let me explain something to you.\"");
+                Console.WriteLine(VENDORLower +"points to the back of the range." );
+                Console.WriteLine(VENDORUpper);
+                Console.WriteLine("\"Ya see that little tin spaceship? You see how it’s not knocked over? Do you know what that means, professor?\"\"\n");
+                Console.WriteLine("The vendor leans over the counter and gets right in Gru’s face.");
+                Console.WriteLine(VENDORUpper);
+                Console.WriteLine("\"It means you don’t get the unicorn!Someone’s got a frowny face. Better luck next time!\"");
+                Console.WriteLine(GRULower + "stares at the vendor, his blood beginning to boil. He remains calm and smiles at the vendor.");
+                Console.WriteLine(GRUUpper);
+                Console.WriteLine("\"Okay, my turn.\"");
+                Console.WriteLine(GRULower + "calmly reaches into his coat and pulls out a small gun that transforms into an enormous RAY GUN. BLAM! A bright blue RAY BLAST connects with the back of the shooting range, leaving no sign of the targets just a giant gaping hole. Gru turns to the vendor.");
+                Console.WriteLine(GRUUpper);
+                Console.WriteLine("\"Knocked over.\"");
+                Console.WriteLine (AGNESUpper + MARGOUpper + EDITHUpper);
+                Console.WriteLine("cheer" + AGNESUpper + "hugs unicorn tightly");
+                Console.WriteLine(AGNESUpper);
+                Console.WriteLine("\"It's so fluffy!\"");
+                Console.WriteLine(EDITHUpper);
+                Console.WriteLine("\"You blew up the whole thing!\"");
+                Console.WriteLine(AGNESUpper);
+                Console.WriteLine("\"Let's go destroy another game!\"");
+                Console.WriteLine("And then it happens: actually connecting with the girls and enjoying being with them, Gru allows a small smile to creep across his face.");}}
+                
+
+                            
+
+
+
+
+
+                
+
+                
+                            
+                
+        
+
+
+
+
+
+
